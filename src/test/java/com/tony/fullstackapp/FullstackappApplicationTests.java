7654779626b0895839a53432f6900b290399c35e
viper23 <htonnycarlos@gmail.com>
2024-12-10 20:10:19 +0200

    initial commit

---
+++ src/test/java/com/tony/fullstackapp/FullstackappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tony.fullstackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
